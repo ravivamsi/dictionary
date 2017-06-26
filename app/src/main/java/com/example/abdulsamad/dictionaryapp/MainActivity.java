30f0f26b60de6b5e45e358fc535f08eeb57f5168
AbdulSammad786 <hasae300@gmail.com>
2017-06-26 11:13:45 +0500

    message

--- app/src/main/java/com/example/abdulsamad/dictionaryapp/MainActivity.java
+++ app/src/main/java/com/example/abdulsamad/dictionaryapp/MainActivity.java
@@ -35,6 +35,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+
 import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     Bitmap image; //our image
